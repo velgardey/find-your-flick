74c74b5a48a0b09230835d69aa7e88d0ba108116
velgardey <arch.velgardey@gmail.com>
2025-01-25 23:25:30 +0530

    minor fixes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,14 @@
 import { PrismaClient } from '@prisma/client'
-import type { Prisma } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
 }
 
-const prismaClientOptions = process.env.NODE_ENV === 'production' 
-  ? { log: ['error'] as Prisma.LogLevel[] }
-  : { log: ['query', 'info', 'warn', 'error'] as Prisma.LogLevel[] }
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  ...prismaClientOptions,
-  datasources: {
-    db: {
-      url: process.env.DIRECT_URL
-    },
-  },
+export const prisma = global.prisma ?? new PrismaClient({
+  log: process.env.NODE_ENV === 'production' 
+    ? undefined
+    : ['query', 'error', 'warn']
 })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma 
